8cbbb7ae91e11921fb7c65fe8e34fbeaaf5f1eb1
Pandurang K <pandurang_k@persistent.com>
2023-04-07 18:57:32 +0530

    update s390x support

---
+++ docker/Toolchain-s390x.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR s390x)
+
+set(cross_triple "s390x-linux-gnu")
+
+set(CMAKE_C_COMPILER /usr/bin/${cross_triple}-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/${cross_triple}-g++)
+set(CMAKE_Fortran_COMPILER /usr/bin/${cross_triple}-gfortran)
+
+# Discard path returned by pkg-config and associated with HINTS in module
+# like FindOpenSSL.
+set(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-s390x-static)
